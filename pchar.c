7faa6eb139a440e37d92c77d7b840f50b4591a7e
SiSimo9750 <bourhim.mo8@gmail.com>
2023-12-18 07:19:17 -0800

    Pushing add and pchar codes

---
+++ pchar.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * f_pchar - prints the char at the stack's top,
+ * followed by a new line
+ * @head: stack head
+ * @count: line_num
+ * Return: nothing
+*/
+void f_pchar(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+
+	h = *head;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
