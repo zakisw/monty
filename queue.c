42a31de9ec2007363a6b5c9b4afcb6232d436e97
zakisw <youssefakisw@gmail.com>
2023-12-17 16:36:23 -0800

    ol

---
+++ queue.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * mu_queue - does print the top
+ * @head: stack head
+ * @counter: line_num
+ * Return: nothing
+*/
+void my_queue(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add node to the stack's tail
+ * @n: new_value
+ * @head: stack's head
+ * Return: nothing
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
